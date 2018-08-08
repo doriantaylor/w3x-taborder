131317b90b18b0392dfa9be6528b1c0604f56555
Dorian Taylor <dorian.taylor.lists@gmail.com>
2018-08-08 12:44:20 -0700

    gonna rip out some comments

--- main.js
+++ main.js
@@ -125,6 +125,82 @@ browser.menus.create({
 
 const SUBMENUS = [];
 
+const TAB_MAP = {};
+
+const CLICKED = {
+};
+const SHOWN = {
+    ttnw: async function (info, tab) {
+        // clear the tab map
+        //console.log(TAB_MAP);
+        for (var tm in TAB_MAP) {
+            await browser.menus.remove(tm);
+            delete TAB_MAP[tm];
+        }
+
+        var url = new URL(tab.url);
+        if (url.hostname !== '') {
+            var d  = url.hostname.toLowerCase().replace(/^www\d*\.+/, '');
+            var dp = d.split(/\.+/);
+
+            console.log(info, dp);
+
+            for (var i = 0; i < dp.length; i++) {
+                var mask = `*.${dp.slice(i).join('.')}`;
+                var base = `ttnw-${i+1}`;
+                var tabs = await browser.tabs.query({ url: `*://${mask}/*` });
+                var mine = [];
+
+                tabs.map(
+                    t => { if (t.windowId === tab.windowId) mine.push(t) });
+                /*
+                for (var j = 0; j < tabs.length; j++) {
+                    if (tabs[j].windowId == tab.windowId) mine.push(tabs[j]);
+                }
+                */
+
+                //console.log(`${base} ${i} ${dp.length}`);
+                var title = mask;
+                if (tabs.length === mine.length) title += ` (${tabs.length})`;
+
+                await browser.menus.create({
+                    id: base, parentId: 'ttnw', title: title,
+                    contexts: ['page', 'tab'] });
+
+                var sm = [];
+                SUBMENUS.push([base]);
+
+                if (tabs.length > mine.length) {
+                    TAB_MAP[base] = [];
+
+                    var v = {
+                        current: [mine, 'From This Window Only'],
+                        all: [tabs, 'From All Windows']
+                    };
+                    for (var j in v) {
+                        var subid = `${base}-${j}`;
+                        TAB_MAP[subid] = v[j][0];
+                        sm.push(subid);
+                        var subtitle = v[j][1] + ` (${v[j][0].length})`;
+                        await browser.menus.create({
+                            id: subid,
+                            parentId: base, title: subtitle,
+                            contexts: ['page', 'tab'] });
+                    }
+                }
+                else {
+                    TAB_MAP[base] = tabs;
+                }
+                console.log(tabs);
+            }
+
+            await browser.menus.update('ttnw', { enabled: true });
+        }
+        await browser.menus.refresh();
+    }
+};
+
+/*
 for (var i = 1; i <= 5; i++) {
     var id = 'ttnw-s' + i;
     SUBMENUS.push(id);
@@ -134,8 +210,58 @@ for (var i = 1; i <= 5; i++) {
         title: 'placeholdurr',
         contexts: ['page', 'tab']
     });
-}
+}*/
 
+browser.menus.onHidden.addListener(async function () {
+    await browser.menus.update('ttnw', { enabled: false });
+    /*
+    await browser.menus.removeAll();
+    browser.menus.create({
+        id: "ttnw", enabled: false,
+        title: 'Tabs to New Window',
+        contexts: ["page", "tab"]
+    }, menuCreated);
+    */
+
+    //console.log(SUBMENUS);
+    /*
+    for (var i = 0; i < SUBMENUS.length; i++) {
+        for (var j = 0; j = SUBMENUS[i].length; j++) {
+            await browser.menus.remove(SUBMENUS[i][j]);
+        }
+    }*/
+    //while (SUBMENUS.length > 0) SUBMENUS.pop();
+
+});
+
+browser.menus.onShown.addListener(function (info, tab) {
+    var id = info.menuIds[0];
+    if (SHOWN[id]) SHOWN[id](info, tab);
+});
+
+browser.menus.onClicked.addListener(async function (info, tab) {
+    console.log(info, tab);
+    var id = info.menuItemId;
+
+    var targets = TAB_MAP[id];
+    
+    if (targets && targets.length > 0) {
+        var mine = await browser.tabs.query({ windowId: tab.windowId });
+
+        console.log(TAB_MAP[id], mine);
+
+        var win;
+        if (mine.length > 1) {
+            win = await browser.windows.create();
+        }
+        else {
+            win = await browser.windows.get(tab.windowId);
+        }
+        //await browser.windows.create();
+    }
+});
+
+/*
 browser.tabs.onActivated.addListener(function (obj) {
     console.log(obj);
     browser.tabs.get(obj.tabId).then(function (fu) {
@@ -152,3 +278,4 @@ browser.tabs.onActivated.addListener(function (obj) {
 
     }, function (wah) { console.error(wah) });
 });
+*/
